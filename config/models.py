c0505010bf4cb0872a91b3a94b7d7e386c3b1334
= <=>
2025-01-25 19:10:18 -0600

    Fixed a few things and added seven point structure

--- config/models.py
+++ config/models.py
@@ -2,7 +2,8 @@
 # FIRST_PASS_GENERATION_MODEL = "Hermes-3-Llama-3.1-8B"
 # FIRST_PASS_GENERATION_MODEL = "llama-3.3-70b-instruct"
 # FIRST_PASS_GENERATION_MODEL = "dolphin-2.9.3-mistral-nemo-12b"
-FIRST_PASS_GENERATION_MODEL = "llama3.1:latest"
+FIRST_PASS_GENERATION_MODEL = "llama3.2:latest"
+# FIRST_PASS_GENERATION_MODEL = "llama-3.2-3b-instruct"
 
 
 SECOND_PASS_EDITOR_MODEL = ""
--- story_writer/story_structures.py
+++ story_writer/story_structures.py
@@ -8,8 +8,8 @@ from story_writer import story_data_model
 
 
 class StoryStructure(Enum):
-    SEVEN_POINT = "Seven Point Story Structure"
     CLASSIC = "Classic"
+    SEVEN_POINT = "Seven Point Story Structure"
     FREYTAGS_PYRAMID = "Freytag's Pyramid"
     THE_HEROS_JOURNEY = "The Hero's Journey"
     DAN_HARMONS_STORY_CIRCLE = "Dan Harmon's Story Circle"
@@ -33,12 +33,12 @@ def get_story_structure_schema(story_structure: StoryStructure) -> dict:
 
 def get_story_structure_model(story_structure: StoryStructure) -> Type[BaseModel]:
     models = {
-        # StoryStructure.SEVEN_POINT: response_schemas.SevenPointStoryStructureData,
+        StoryStructure.SEVEN_POINT: story_data_model.SevenPointStoryStructure,
         StoryStructure.CLASSIC: story_data_model.ClassicStoryStructure,
-        # StoryStructure.FREYTAGS_PYRAMID: response_schemas.FreytagsPyramidData,
-        # StoryStructure.THE_HEROS_JOURNEY: response_schemas.TheHerosJourneyData,
-        # StoryStructure.DAN_HARMONS_STORY_CIRCLE: response_schemas.DanHarmonsStoryCircleData,
-        # StoryStructure.FICHTEAN_CURVE: response_schemas.FichteanCurveData,
-        # StoryStructure.SAVE_THE_CAT: response_schemas.SaveTheCatData,
+        # StoryStructure.FREYTAGS_PYRAMID: story_data_model.FreytagsPyramidData,
+        # StoryStructure.THE_HEROS_JOURNEY: story_data_model.TheHerosJourneyData,
+        # StoryStructure.DAN_HARMONS_STORY_CIRCLE: story_data_model.DanHarmonsStoryCircleData,
+        # StoryStructure.FICHTEAN_CURVE: story_data_model.FichteanCurveData,
+        # StoryStructure.SAVE_THE_CAT: story_data_model.SaveTheCatData,
     }
     return models.get(story_structure, story_data_model.ClassicStoryStructure)
