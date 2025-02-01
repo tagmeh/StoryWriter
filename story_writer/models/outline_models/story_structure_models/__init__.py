9356e8835191d83af95ad0b74f5f6f7c7e5860f6
= <=>
2025-01-31 22:28:26 -0600

    Removed Stakes. Moved story_structure_model/ dir.

---
+++ story_writer/models/outline_models/story_structure_models/__init__.py
@@ -0,0 +1,15 @@
+from story_writer.models.outline_models.story_structure_models.save_the_cat import SaveTheCatStructure
+
+__all__ = [
+    "ClassicStoryStructure",
+    "DanHarmonsStoryCircleStructure",
+    "FichteanCurveStructure",
+    "FiveActStructure",
+    "FreytagsPyramidStoryStructure",
+    "InMediasRes",
+    "SaveTheCatStructure",
+    "SevenPointStoryStructure",
+    "StorySpine",
+    "TheHerosJourneyStoryStructure",
+    "ThreeActStructure",
+]
